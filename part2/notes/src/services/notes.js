254703db6aea1184b69fab99c94202f45b2b90c9
KurbyDoo <106122181+KurbyDoo@users.noreply.github.com>
2024-10-14 22:10:19 -0400

    updated port

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://notes-app-fsopen.onrender.com'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
--- part3/notes/index.js
+++ part3/notes/index.js
@@ -80,5 +80,6 @@ app.post('/api/notes', (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server is running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
